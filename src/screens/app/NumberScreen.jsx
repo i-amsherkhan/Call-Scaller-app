bfc20635fad608c3136efcafa21e8c3badbb06df
Sher Khan <sher.khan@cruisebrains.com>
2022-11-22 16:32:32 +0500

    Adding Api functionality

--- src/screens/app/NumberScreen.jsx
+++ src/screens/app/NumberScreen.jsx
@@ -1,15 +1,19 @@
-import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, FlatList } from "react-native";
+import React, { useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "../../components/Header";
-
+import { ApiContext } from "../../context/ApiProvider";
 import { DataTable } from "react-native-paper";
 
 const NumberScreen = ({ navigation }) => {
+  const { testing } = useContext(ApiContext);
+  console.log(testing);
+
   return (
     <SafeAreaView>
       <ScrollView>
         <Header navigation={navigation} />
+
         <View className="bg-white  px-4 py-5 border-b border-gray-200 rounded-lg shadow mx-2 mt-2">
           <Text className="text-lg leading-6 text-gray-900">Numbers</Text>
         </View>
@@ -17,7 +21,7 @@ const NumberScreen = ({ navigation }) => {
         <ScrollView horizontal={true}>
           <DataTable className="px-2 flex-1 h-full">
             <DataTable.Header className="bg-gray-50 mt-6  border-spacing-36 border-gray-300 rounded-lg shadow-lg">
-              <DataTable.Title className="truncate w-36">
+              <DataTable.Title className="truncate w -36">
                 <Text className="text-left text-gray-500 uppercase tracking-wider">
                   Phone number
                 </Text>
@@ -58,14 +62,23 @@ const NumberScreen = ({ navigation }) => {
                 </View>
               </DataTable.Cell>
 
-              <DataTable.Cell className="w-24 right-3">
-                <Text className="truncate text-gray-900 text-center">
-                  Sher Khan Call Flows
-                </Text>
-              </DataTable.Cell>
+            
+              <FlatList
+                keyExtractor={(item) => item.id} 
+                data={testing}
+                renderItem={({ item }) => (
+                  <>
+                    <DataTable.Cell className="w-24 mt-2">
+                      <Text className="truncate text-gray-900 text-center">
+                        {item.name}
+                      </Text>
+                    </DataTable.Cell>
+                  </>
+                )}
+              />
 
               <DataTable.Cell className="truncate w-32 left-4">
-                <Text className=" text-gray-900 text-center">40000</Text>
+                <Text className=" text-gray-900 text-center">4000</Text>
               </DataTable.Cell>
 
               <DataTable.Cell className="truncate w-20">
@@ -75,7 +88,7 @@ const NumberScreen = ({ navigation }) => {
               <DataTable.Cell className="truncate w-20">
                 <View>
                   <View>
-                    <Text className=" text-gray-900 text-center">Active</Text>
+                    <Text className="text-gray-900 text-center">Active</Text>
                   </View>
                   <View className="pt-2">
                     <Text className=" text-gray-900 text-center">
